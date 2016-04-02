dd9e1e2f45fc23b8a774443a5f1460699fb80987
gunner <gymgunner@gmail.com>
2016-04-02 17:38:08 +0800

    Add crontab nginx confs

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def xchange():
     return render_template('index.html', rate_dict=rate_dict)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
---
+++ crontab.sh
@@ -0,0 +1,1 @@
+*/1  *  * * * . /home/gunner/workspace/xchange/bin/activate && cd /home/gunner/workspace/xchange/ && python get_xchange.py
